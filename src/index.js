9775a6c6a458e53d52c770b2facbd14cc3d531d2
Larry Schirmer <larryschirmer@Larrys-MBP.illuminatingconcepts.local>
2018-05-14 11:22:54 -0400

    add basic navigation to slides

--- src/index.js
+++ src/index.js
@@ -1,5 +1,33 @@
-import React from "react";
+import React, { Component } from "react";
 import ReactDOM from "react-dom";
-import { Fangirling, Button } from "./Slides";
 
-ReactDOM.render(<Button />, document.getElementById("root"));
+import { Fangirling, Button } from "./Slides";
+import { Wrapper, Nav, NavItem, View } from "./styles";
+
+class MainPage extends Component {
+  state = {
+    activePage: "fangirling"
+  };
+
+  onSelect = itemName => () => {
+    this.setState({ activePage: itemName });
+  };
+
+  render() {
+    const { activePage } = this.state;
+    return (
+      <Wrapper>
+        <Nav>
+          <NavItem onClick={this.onSelect("fangirling")}>Fangirling</NavItem>
+          <NavItem onClick={this.onSelect("button")}>Button</NavItem>
+        </Nav>
+        <View>
+          {activePage === "fangirling" && <Fangirling />}
+          {activePage === "button" && <Button />}
+        </View>
+      </Wrapper>
+    );
+  }
+}
+
+ReactDOM.render(<MainPage />, document.getElementById("root"));
--- src/styles.js
+++ src/styles.js
@@ -2,22 +2,23 @@ import styled from "styled-components";
 
 export const Wrapper = styled.div`
   display: grid;
-  grid-template-columns: 1fr 1fr;
+  grid-template-rows: 75px 600px;
+
+  font-family: 'Roboto', sans-serif;
 `;
 
-export const ButtonSection = styled.div`
-  grid-column: 2/3;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-`;
-
-export const Button = styled.div`
+export const Nav = styled.div`
+  grid-row: 1/2;
   display: flex;
   flex-direction: row;
-  justify-content: center;
+  justify-content: flex-start;
   align-items: center;
+  font-size: 18px;
+`;
+
+export const NavItem = styled.div`
+  margin-left: 10px;
+
   padding: 5px 10px;
   border: 0.5px solid black;
   border-radius: 3px;
@@ -34,4 +35,11 @@ export const Button = styled.div`
   &:active {
     background: rgba(170, 170, 170, 1);
   }
+`;
+
+export const View = styled.div`
+  grid-row: 2/3;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
